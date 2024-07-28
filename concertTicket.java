264fb0d6e1805e8a61cec99a4a50ebbbea6a21bc
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-07-28 17:34:16 +0300

    adding getValue and setValue methods for Ticket parameters

--- concertTicket.java
+++ concertTicket.java
@@ -18,13 +18,13 @@ public class concertTicket extends Ticket {
     public void printTicket() {
         System.out.println("This is a ticket to the concert.");
         System.out.println("Ticket ID: " + getID());
-        System.out.println("Ticket creation time is: " + creationTime);
-        System.out.println("Concert hall is: " + concertHall);
-        System.out.println("Event code is: " + eventCode);
-        System.out.println("Event time is: " + eventTime);
-        System.out.println("Ticket is promo:" + isPromo);
-        System.out.println("Stadium sector is: " + stadiumSector);
-        System.out.println("Max allowed backpack weight: " + String.format("%.2f", maxWeight) + " kg.g");
-        System.out.println("Price=" + price + " EUR");
+        System.out.println("Ticket creation time is: " + getCreationTime());
+        System.out.println("Concert hall is: " + getConcertHall());
+        System.out.println("Event code is: " + getEventCode());
+        System.out.println("Event time is: " + getEventTime());
+        System.out.println("Ticket is promo:" + getIsPromo());
+        System.out.println("Stadium sector is: " + getStadiumSector());
+        System.out.println("Max allowed backpack weight: " + String.format("%.2f", getMaWeight()) + " kg.g");
+        System.out.println("Price=" + getPrice() + " EUR");
     }
 }
